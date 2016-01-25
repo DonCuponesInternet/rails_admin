6b8edcfad2baeb8d5d03da219f4fdf112b7be8f7
vemv <vemv@vemv.net>
2016-01-26 00:58:57 +0100

    Add informational classes, for facilitating testing

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -175,11 +175,11 @@ module RailsAdmin
       actions = actions(:bulkable, abstract_model)
       return '' if actions.empty?
       content_tag :li, class: 'dropdown', style: 'float:right' do
-        content_tag(:a, class: 'dropdown-toggle', data: {toggle: 'dropdown'}, href: '#') { t('admin.misc.bulk_menu_title').html_safe + ' ' + '<b class="caret"></b>'.html_safe } +
+        content_tag(:a, class: 'dropdown-toggle bulk-menu-toggler', data: {toggle: 'dropdown'}, href: '#') { t('admin.misc.bulk_menu_title').html_safe + ' ' + '<b class="caret"></b>'.html_safe } +
           content_tag(:ul, class: 'dropdown-menu', style: 'left:auto; right:0;') do
             actions.collect do |action|
               content_tag :li do
-                link_to wording_for(:bulk_link, action), '#', onclick: "jQuery('#bulk_action').val('#{action.action_name}'); jQuery('#bulk_form').submit(); return false;"
+                link_to(wording_for(:bulk_link, action), '#', class: "bulk-action-trigger-#{action.action_name}", onclick: "jQuery('#bulk_action').val('#{action.action_name}'); jQuery('#bulk_form').submit(); return false;")
               end
             end.join.html_safe
           end
